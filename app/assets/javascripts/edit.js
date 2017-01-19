ec9a06d5323397978511bfec0277420e78e792d7
Karina Shedlbauer <k@rina.wtf>
2017-01-19 12:47:12 +0300

    Fastfix edit.js

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -8,6 +8,6 @@ function avant_add_clone() {
 
 function avant_add() {
   var avant = document.createElement('figure');
-    avant.innerHTML = '<aside><img src="/assets/arrow.jpg" alt="Arrow"><input name="avantages[][icon_new]" type="file" id="item_icon_new"></aside><figcaption><a id="avant_del" onclick="this.parentNode.parentNode.remove()">[x] Удалить характеристику</a><input name="avantages[][icon]" type="hidden" value="arrow.jpg"><label>заголовок</label><input class="title" name="avantages[][title]" type="text" value="ХАРАКТЕРИСТИКА"><label>описание</label><input class="description" name="avantages[][description]" type="text" value="описание"></figcaption>';
+    avant.innerHTML = '<aside><img src="/assets/arrow.jpg" alt="Arrow"><input name="avantages[][icon_new]" type="file" id="item_icon_new"></aside><figcaption><a id="avant_del" onclick="this.parentNode.parentNode.remove()">[x] Удалить</a><input name="avantages[][icon]" type="hidden" value="arrow.jpg"><label>заголовок</label><input class="title" name="avantages[][title]" type="text" value="ХАРАКТЕРИСТИКА"><label>описание</label><input class="description" name="avantages[][description]" type="text" value="описание"></figcaption>';
   avants.appendChild(avant);
 };
