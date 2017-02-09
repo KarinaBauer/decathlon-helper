67ef8eb34a768eebad5ed2e1e04a0738f9073133
Karina Shedlbauer <k@rina.wtf>
2017-02-09 13:04:47 +0300

    v.2.0 Beta, stable material beta

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -2,4 +2,8 @@ class StartController < ApplicationController
   def index
   	$url = ''
   end
+
+  def about
+  	render 'about'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   resources :icons
-  get '/edit', to: 'balisage#edit'
+  get '/about', to: 'start#about'
   post '/edit', to: 'balisage#edit'
   get '/edit', to: 'balisage#edit'
   post '/view', to: 'balisage#generate'
   get '/view', to: 'balisage#generate'
-  #post '/generated', to: 'generated#preview'
-  #get '/generated', to: 'generated#preview'
   root 'start#index'
 end
