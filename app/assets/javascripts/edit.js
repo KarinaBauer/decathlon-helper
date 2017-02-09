5b6b1cb0da27202b0f0c7e824336c3672626e975
Karina Shedlbauer <k@rina.wtf>
2017-02-09 14:13:22 +0300

    v.2.2 Beta

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -2,10 +2,11 @@ function avant_add() {
   var avant = document.createElement('figure');
   avant.innerHTML = 
     	'<aside>\
-    		<img src="assets/arrow.jpg" alt="Arrow" />\
+    		<label>Иконка</label>\
+    		<img class="icon" src="assets/arrow.jpg" alt="Arrow" />\
     	 </aside>\
     	 <figcaption>\
-    		<a class="button-delete" onclick="this.parentNode.parentNode.remove()">[x] Удалить</a>\
+    		<a class="button button_small" onclick="this.parentNode.parentNode.remove()">[x] Удалить</a>\
     		<input name="item[avantages][][icon]" type="hidden" value="arrow.jpg">\
     		<label>Заголовок</label>\
     		<input class="title" name="item[avantages][][title]" type="text" value="ХАРАКТЕРИСТИКА">\
