a649a0e20e73021330cfd1273a7d3c8abbc72fb1
Karina Shedlbauer <k@rina.wtf>
2017-02-20 18:56:08 +0300

    v.2.8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  $std_icon = 'arrow.jpg'
+  $std_icon = '/assets/arrow.jpg'
 	$item_data = { articulus: 12345678, name: 'НЕТ ВХОДНЫХ ДАННЫХ', price: 0, image: 'img.jpg' }
 	$avantages_null = [ {icon: $std_icon, title: 'НЕТ ДАННЫХ', description: 'данных не обнаружено'} ]
 end
